da7170942e158e78bd1de8b8dc6131982137f204
Ansel Gamet <ansel.gamet@telecomnancy.net>
2018-12-04 11:53:13 +0100

    cleanup

--- src/plotting.py
+++ src/plotting.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 
 ONE_HOUR = numpy.timedelta64(1,'h')
 ONE_YEAR = numpy.timedelta64(365,'D')
+GRAPHS_PATH = '../graphs/'
 
 figureCount = 0
 
@@ -18,61 +19,49 @@ datatype = [('Timestamp',numpy.datetime64('1970-01-01T00:00:00')),
             ('Text_Avg',numpy.float32),
             ('WD_MeanUnitVector',numpy.float32),
             ('WS_Mean',numpy.float32)]
-foldername='../Data/Moufia_2014_2015/'
-filename ='moufia_2014_2015.csv'
+foldername = '../Data/Moufia_2014_2015/'
+filename = 'moufia_2014_2015.csv'
 filepath = foldername + filename
 
-def average(timeline, values, scale = ONE_HOUR):
-    averagedValues = []
-    simplifiedTimeline = []
-    i = 0
-    while i<len(timeline):
-        summedValues = 0.0
-        valueCount = 0.0
-        hour = timeline[i]
-        while i<len(timeline) and timeline[i]<hour+scale:
-            summedValues += values[i]
-            valueCount += 1
-            i += 1
-        averagedValues.append(summedValues/valueCount)
-        simplifiedTimeline.append(hour)
-    
-    return simplifiedTimeline, averagedValues
 
-def drawBoxPlot(data, title=''):
-    global figureCount
-    x = []
-    x.append(data[:522592])
-    x.append(data[522592:])
+def average(timeline, values, scale=ONE_HOUR):
+    averaged_values = []
+    simplified_timeline = []
+    i = 0
+    while i < len(timeline):
+        summed_values = 0.0
+        value_count = 0.0
+        hour = timeline[i]
+        while i < len(timeline) and timeline[i] < hour+scale:
+            summed_values += values[i]
+            value_count += 1
+            i += 1
+        averaged_values.append(summed_values/value_count)
+        simplified_timeline.append(hour)
     
-    figureCount+=1
+    return simplified_timeline, averaged_values
+
+
+def draw_box_plot(data, title=''):
+    global figureCount
+    x = [data[:522592], data[522592:]]
+    
+    figureCount += 1
     plt.figure(figureCount)
     
     plt.boxplot(x, sym='')
     plt.title(title)
     plt.draw()
+    plt.savefig(GRAPHS_PATH + title + '.png')
+
 
 def main():    
     data = numpy.genfromtxt(filepath, dtype=datatype, delimiter=',', names=True)
-    #houraveraged = numpy.mean(data['FD_Avg'][:1022400].reshape(-1,1440), axis=1)
-    #plt.plot(houraveraged[:365])
-    
-    #x, y = average(data['Timestamp'], data['Patm_Avg'], ONE_YEAR)
-    
-    drawBoxPlot(data['FD_Avg'],'FD_Avg')
-    drawBoxPlot(data['FG_Avg'],'FG_Avg')
-    drawBoxPlot(data['Patm_Avg'],'Patm_Avg')
-    
-    #plt.plot(x, y,'r,')    
-    #plt.plot(data['Timestamp'],data['FG_Avg'],'b,')
-    #plt.grid(True)
-    #plt.title("Direct illumination")
-    #plt.xlabel("Timestamp")
-    #plt.ylabel("FD_Avg")
-    #plt.savefig('FD_Avg.png')
-    
-    #plt.draw()
-    
+
+    draw_box_plot(data['FD_Avg'],'FD_Avg')
+    draw_box_plot(data['FG_Avg'],'FG_Avg')
+    draw_box_plot(data['Patm_Avg'],'Patm_Avg')
+
 
 if __name__ == "__main__":
     main()
